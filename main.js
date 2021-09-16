f0907fc63a44935674248159e78f985ed53368d5
Christine Tina Park <escp1004jk@gmail.com>
2021-09-16 15:57:59 -0700

    Completed issue-3-start-a-car. - Christine Tina Park

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var $car = document.querySelector('#car');
-var turnList = ['turn0', 'turn90', 'turn180', 'turn270'
-];
+var turnList = ['turn0', 'turn90', 'turn180', 'turn270'];
+var intervalID;
+var carLocation = {
+  x: 0,
+  y: 0
+};
 
 window.addEventListener('keydown', handleKeyDown);
+window.addEventListener('keyup', handleKeyUp);
 
 function doTurn(turnName) {
   for (var i = 0; i < turnList.length; i++) {
@@ -21,5 +26,27 @@ function handleKeyDown(event) {
     doTurn('turn180');
   } else if (event.key === 'ArrowUp') {
     doTurn('turn270');
+  } else if (event.code === 'Space') {
+    startMoving();
   }
+}
+
+function handleKeyUp(event) {
+  if (event.code === 'Space') {
+    stopMoving();
+  }
+}
+
+function moveCar() {
+  var styleText = 'top: ' + carLocation.y + 'px; left: ' + carLocation.x + 'px';
+  $car.setAttribute('style', styleText);
+  carLocation.x += 7;
+}
+
+function startMoving() {
+  intervalID = setInterval(moveCar, 16);
+}
+
+function stopMoving() {
+  clearInterval(intervalID);
 }
